086c014ca11853600386c0f662979a6ead74f778
MarcosWinther <winthermarcos@gmail.com>
2024-02-24 21:53:38 -0300

    Configurado o reset CSS e criado o componente Page List

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Pages
+import { ListComponent } from './modules/to-do-list/pages/list/list.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  }
+];
