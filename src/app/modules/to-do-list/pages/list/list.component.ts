f0e5182514695a981c1277f23fcb3bda1fe08e8f
MarcosWinther <winthermarcos@gmail.com>
2024-02-26 12:19:21 -0300

    TS do Page List Component

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-
+  public addItem = signal(true);
 }
