744b9d652e38c642dafb86c993e4b46b8da76a3f
MarcosWinther <winthermarcos@gmail.com>
2024-02-27 23:37:40 -0300

    CSS do page list component

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -28,4 +28,9 @@ export class ListComponent {
 
     return this.#setListItems.set(this.#parseItems());
   }
+
+  public deleteAllItems() {
+    localStorage.removeItem('@my-list');
+    return this.#setListItems.set(this.#parseItems());
+  }
 }
